1cad641244a5e21947f0c16db0d4ccbfb07e0389
alpallel <alpallel.e@gmail.com>
2026-09-16 19:14:02 +0700

    fix: add PWS on allowed hosts

--- EcoCertificationCompany/settings.py
+++ EcoCertificationCompany/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0u$&o6)^y^wvo&k(+ctk2y#gh=cbggasn0qho597pv)=t)j5kt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'alfarrel-ersya-bhumilestari-mockapi.pws.cs.ui.ac.id']
 
 
 # Application definition
