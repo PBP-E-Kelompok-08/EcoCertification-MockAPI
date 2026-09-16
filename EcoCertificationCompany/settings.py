a3691dfe2cd801f5e93bac619d431a6ea69d9494
alpallel <alpallel.e@gmail.com>
2026-09-16 19:18:42 +0700

    fix: update CSRF trusted origins for mock API

--- EcoCertificationCompany/settings.py
+++ EcoCertificationCompany/settings.py
@@ -141,5 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "https://alfarrel-ersya-myportofolio.pws.cs.ui.ac.id",
+    "alfarrel-ersya-bhumilestari-mockapi.pws.cs.ui.ac.id",
 ]
